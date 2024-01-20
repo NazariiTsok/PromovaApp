a95aeffd87f89c82c78b280270932ffa9329e79c
Nazarii Tsok <nazarii@tryon.technology>
2024-01-20 21:20:50 +0200

    Initial Commit

---
+++ PromovaApp/PromovaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromovaAppApp.swift
+//  PromovaApp
+//
+//  Created by Nazar Tsok on 20.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct PromovaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
